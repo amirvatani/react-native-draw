2e944eced3a20bad75f98a7da79540639d4cfd50
amirvatani <amirhosseinvatani75@gmail.com>
2023-03-25 21:18:09 +0330

    done

--- packages/react-native-draw-extras/src/icons/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Delete } from './Delete';
-export { default as Palette } from './Palette';
-export { default as Brush } from './Brush';
-export { default as Undo } from './Undo';
-export { default as Eraser } from './Eraser';
